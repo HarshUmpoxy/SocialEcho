453ebfcba920f7547656f0e4bb0689565754ae09
HarshUmpoxy <harshgarg2550@gmail.com>
2023-12-09 17:13:58 +0530

    correct image path

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="flex items-center ">
       <b>Social Echo :</b>
-      <img src="/src/assets/logo.jpg" className="h-12 w-12 rounded-xl" alt="Social Echo Logo" />
+      <img src="./src/assets/logo.jpg" className="h-12 w-12 rounded-xl" alt="Social Echo Logo" />
     </div>
 
   )
